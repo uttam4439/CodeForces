8bc26b24c64361e102a13b400d8915a1c1cde41d
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-12-29 21:19:58 +0530

    Correct one

--- BeautifulYear.java
+++ BeautifulYear.java
@@ -4,30 +4,48 @@ public class BeautifulYear{
     public static void main(String[] args) {
         
         Scanner gg = new Scanner(System.in);
-        
         int y = gg.nextInt();
-        int res = 0;
 
-        Map<Integer,Integer> kk = new HashMap<>();
-        if(y >= 1000 && y <= 9000){
-            int x = y;
-            while(x > 0){ 
-                int dig = x % 10; 
-                kk.put(dig, kk.getOrDefault(dig,0)+1);
-                x = x/10;
-            }
-            boolean allUnique = true;
-            for(int i : kk.values()){
-                if(i != 1){
-                     allUnique = false;
-                     break;
-                }
-            }
-            if(allUnique == true){
-                res = y+1;
+        // int res = 0;
+        // Map<Integer,Integer> kk = new HashMap<>();
+        // if(y >= 1000 && y <= 9000){
+        //     int x = y;
+        //     while(x > 0){ 
+        //         int dig = x % 10; 
+        //         kk.put(dig, kk.getOrDefault(dig,0)+1);
+        //         x = x/10;
+        //     }
+        //     boolean allUnique = true;
+        //     for(int i : kk.values()){
+        //         if(i != 1){
+        //              allUnique = false;
+        //              break;
+        //         }
+        //     }
+        //     if(allUnique == true){
+        //         res = y+1;
+        //     }
+        // }
+        // System.out.println(res);
+
+        while(true){
+            y++;
+            if(isBeautiful(y)){
+                System.out.println(y);
+                break;
             }
         }
-        System.out.println(res);
+    }
+    static boolean isBeautiful(int y){
+        Set<Integer> kk = new HashSet<>();
 
+        while(y > 0){
+            int d = y % 10;
+            if(kk.add(d) == false){
+                return false;
+            }
+            y = y /10;
+        }
+        return true;
     }
 }
